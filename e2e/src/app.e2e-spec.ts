b57b785df694a272ee9bd124564c5ee10ee5cb82
Serhii Vabishchevych <sergiy.vabischevych@gmail.com>
2019-06-20 16:03:13 +0300

    changed e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display default time', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to timer!');
+    expect(page.getTitleText()).toEqual('00:00:00');
   });
 
   afterEach(async () => {
