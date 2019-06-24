a3f6df38776ca1fc8d07f6722d428132770b1e8c
Serhii Vabishchevych <sergiy.vabischevych@gmail.com>
2019-06-24 14:24:43 +0300

    improved e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,78 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, ElementFinder } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
-describe('workspace-project App', () => {
+describe('timer-project App Component', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
   it('should display default time', () => {
-    page.navigateTo();
     expect(page.getTitleText()).toEqual('00:00:00');
+  });
+
+  it('should display 3 buttons', () => {
+    expect(page.getButtons().count()).toBe(3);
+  });
+
+  it('should start/stop button work', () => {
+    const expectedTime = '00:00:05';
+    page.getButtonStart().click();
+    browser.waitForAngularEnabled(false);
+    browser.wait(
+      () => page.getTitleText().then((result: string) => result === expectedTime)
+    );
+    page.getButtonStop().click();
+    expect(page.getButtonStart().getText()).toEqual('Start');
+    expect(page.getTitleText()).toEqual(expectedTime);
+
+    // second start timer
+    page.getButtonStart().click();
+    expect(page.getButtonStop().getText()).toEqual('Stop');
+    expect(page.getTitleText()).toEqual('00:00:06');
+  });
+
+  it('should reset button work', () => {
+    // start timer
+    const expectedTime = '00:00:03';
+    page.getButtonStart().click();
+    browser.waitForAngularEnabled(false);
+    browser.wait(
+      () => page.getTitleText().then((result: string) => result === expectedTime)
+    );
+    expect(page.getTitleText()).toEqual(expectedTime);
+
+    // reset timer
+    page.getButtonReset().click();
+    expect(page.getTitleText()).toEqual('00:00:00');
+  });
+
+  it('should wait button work', () => {
+    // start timer
+    const expectedTime = '00:00:03';
+    page.getButtonStart().click();
+    browser.waitForAngularEnabled(false);
+    browser.wait(
+      () => page.getTitleText().then((result: string) => result === expectedTime)
+    );
+    expect(page.getButtonWait().getText()).toEqual('Wait');
+    expect(page.getTitleText()).toEqual(expectedTime);
+
+    // wait button clicked
+    page.getButtonWait().click();
+    browser.sleep(301);
+    page.getButtonWait().click();
+    expect(page.getTitleText()).toEqual(expectedTime);
+
+    // wait button dbclicked
+    browser.sleep(2000);
+    page.getButtonWait().click();
+    browser.sleep(290);
+    page.getButtonWait().click();
+    expect(page.getTitleText()).toEqual('00:00:05');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,36 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+
+  navigateTo(): Promise<any> {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root div.timer')).getText() as Promise<string>;
+  getTitleText(): Promise<any> {
+    return this.getAppRootContainer().element(by.css('div.timer')).getText() as Promise<string>;
+  }
+
+  getButtons(): ElementArrayFinder {
+    return this.getAppRootContainer().all(by.css('div.btn-group button'));
+  }
+
+  getButtonStart(): ElementFinder {
+    return this.getAppRootContainer().element(by.css('button.btn-start'));
+  }
+
+  getButtonStop(): ElementFinder {
+    return this.getAppRootContainer().element(by.css('button.btn-stop'));
+  }
+
+  getButtonReset(): ElementFinder {
+    return this.getAppRootContainer().element(by.css('button.btn-reset'));
+  }
+
+  getButtonWait(): ElementFinder {
+    return this.getAppRootContainer().element(by.css('button.btn-wait'));
+  }
+
+  private getAppRootContainer(): ElementFinder {
+    return element(by.css('app-root'));
   }
 }
